f04d1934809fa2cd5471c681daaa3f6b6c1741b6
Jian Han <jianhan@mail.com>
2018-09-16 08:59:52 +1000

    [feat] API calling for nearby search works now

--- handler/google.go
+++ handler/google.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jianhan/gogin/error"
+	"github.com/jianhan/gogin/google"
+	"googlemaps.github.io/maps"
 	"gopkg.in/go-playground/validator.v9"
 	"net/http"
 )
 
 type googleNearbySearchRequest struct {
-	Lat       float64 `form:"lat" json:"lat" validate:"required,latitude"`
-	Lon       float64 `form:"lon" json:"lon" validate:"required,longitude"`
+	Lat       float64 `form:"lat" json:"lat" validate:"required"`
+	Lng       float64 `form:"lng" json:"lon" validate:"required"`
 	Radius    uint    `form:"radius" json:"radius" validate:"required,gte=100,lte=10000"`
 	Keyword   string  `form:"keyword" json:"keyword"`
 	MinPrice  uint    `form:"min_price" json:"min_price" validate:"gte=0,lte=4"`
 	MaxPrice  uint    `form:"max_price" json:"max_price" validate:"gte=0,lte=4"`
+	OpenNow   bool    `form:"open_now" json:"open_now,omitempty"`
 	RankBy    string  `form:"rankby" json:"rankby" validate:"oneof=prominence distance"`
 	PageToken string  `form:"page_token" json:"page_token"`
 }
@@ -43,5 +46,30 @@ func googleNearbySearch(c *gin.Context) {
 	}
 
 	// make API request
-	c.String(200, "pong")
+	client, err := google.MapClient()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, &error.APIError{Status: http.StatusInternalServerError, Details: err.Error()})
+		return
+	}
+
+	var searchRequest *maps.NearbySearchRequest
+	if req.PageToken != "" {
+		searchRequest = &maps.NearbySearchRequest{PageToken: req.PageToken}
+	} else {
+		searchRequest = &maps.NearbySearchRequest{
+			Location: &maps.LatLng{Lat: req.Lat, Lng: req.Lng},
+			Radius:   req.Radius,
+			MinPrice: maps.PriceLevel(req.MinPrice),
+			MaxPrice: maps.PriceLevel(req.MaxPrice),
+			Type:     maps.PlaceType("restaurant"),
+		}
+	}
+
+	response, err := client.NearbySearch(c, searchRequest)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, &error.APIError{Status: http.StatusInternalServerError, Details: err.Error()})
+		return
+	}
+
+	c.JSON(200, response.Results)
 }

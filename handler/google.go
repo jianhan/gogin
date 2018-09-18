f758644ed48804ba1933a8dc5d6dd026af59757c
Jian Han <jianhan@mail.com>
2018-09-18 20:04:26 +1000

    [refactor] Updated nearby search handler

--- handler/google.go
+++ handler/google.go
@@ -3,29 +3,35 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	gerr "github.com/jianhan/gogin/error"
+	"github.com/jianhan/gogin/google"
 	"github.com/leebenson/conform"
 	"net/http"
 )
 
-type googleNearbySearchRequest struct {
-	Lat       float64 `form:"lat" json:"lat" validate:"required"`
-	Lng       float64 `form:"lng" json:"lon" validate:"required"`
-	Radius    uint    `form:"radius" json:"radius" validate:"required,gte=100,lte=10000"`
-	Keyword   string  `conform:"trim" form:"keyword" json:"keyword"`
-	MinPrice  uint    `form:"min_price" json:"min_price" validate:"gte=0,lte=4"`
-	MaxPrice  uint    `form:"max_price" json:"max_price" validate:"gte=0,lte=4"`
-	OpenNow   bool    `form:"open_now" json:"open_now,omitempty"`
-	RankBy    string  `conform:"trim" form:"rankby" json:"rankby" validate:"oneof=prominence distance"`
-	PageToken string  `conform:"trim" form:"page_token" json:"page_token"`
+type GoogleAPIHandler struct {
+	nearbySearch google.NearbySearch
 }
 
-func googleNearbySearch(c *gin.Context) {
-	var req googleNearbySearchRequest
+func (g *GoogleAPIHandler) NearbySearch(c *gin.Context) {
+	// generate request
+	var req google.NearbySearchRequest
 	if err := c.ShouldBindQuery(&req); err != nil {
 		c.JSON(http.StatusBadRequest, &gerr.APIError{Details: err.Error()})
 		return
 	}
 	conform.Strings(&req)
+
+	res, status, err := g.nearbySearch.Search(c, &req)
+	if err != nil {
+		c.JSON(status, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+	return
+}
+
+func googleNearbySearch(c *gin.Context) {
 
 	c.JSON(200, response.Results)
 }
